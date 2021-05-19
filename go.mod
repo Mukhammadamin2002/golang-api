3029b6a2c5b778d89c5cfb480c7a176d813ef531
Mukhammadamin <72244353+Mukhammadamin2002@users.noreply.github.com>
2021-05-20 01:38:08 +0500

    CRUD  created with Postgresql (Go)

---
+++ go.mod
@@ -0,0 +1,8 @@
+module tasks
+
+go 1.16
+
+require (
+	github.com/gorilla/mux v1.8.0
+	github.com/jinzhu/gorm v1.9.16
+)
--- main.go
+++ main.go
@@ -5,41 +5,109 @@ import (
 	"fmt"
 	"log"
 	"net/http"
+	"os"
+
+	"github.com/gorilla/mux"
+	"github.com/jinzhu/gorm"
+	_ "github.com/jinzhu/gorm/dialects/postgres" // here
 )
 
-type Task struct {
-	Title    string `json:"Title"`
-	Duration string `json:"duration"`
-	Date     string `json:"date"`
+type Contact struct {
+	gorm.Model
+
+	Name    string
+	Email   string `gorm:"typevarchar(100);unique_index"`
+	Address string
 }
 
-type Tasks []Task
-
-func allTasks(w http.ResponseWriter, r *http.Request) {
-	tasks := Tasks{
-		Task{Title: "Housework", Duration: "2 hour", Date: "Monday"},
-		Task{Title: "Laisure Activity", Duration: "1 hour", Date: "Tuesday"},
-		Task{Title: "Go Suburban Area", Duration: "3:30 hour", Date: "Wednesday"},
-		Task{Title: "Holiday", Duration: "whole day", Date: "Friday"},
-		Task{Title: "Climbing", Duration: "3 hour", Date: "Thursday"},
-		Task{Title: "Helping Other People", Duration: "1:30 hour", Date: "Saturday"},
-		Task{Title: "Fishing", Duration: "1:50 hour", Date: "Sunday"},
+var (
+	contact = &Contact{
+		Name: "Alex", Email: "aliaaa@gmail.com", Address: "New York",
 	}
+)
 
-	fmt.Println("Endpoint Hit: All tasks Endpoint")
-	json.NewEncoder(w).Encode(tasks)
-}
-
-func homePage(w http.ResponseWriter, r *http.Request) {
-	fmt.Fprintf(w, "Homepage Endpoint Hit")
-}
-
-func handleRequests() {
-	http.HandleFunc("/", homePage)
-	http.HandleFunc("/tasks", allTasks)
-	log.Fatal(http.ListenAndServe(":8080", nil))
-}
+var db *gorm.DB
+var err error
 
 func main() {
-	handleRequests()
+	dialect := os.Getenv("DIALECT")
+	host := os.Getenv("HOST")
+	dbPort := os.Getenv("DBPORT")
+	user := os.Getenv("USER")
+	dbName := os.Getenv("NAME")
+	password := os.Getenv("PASSWORD")
+
+	dbURI := fmt.Sprintf("host=%s user=%s dbname=%s sslmode=disable password=%s port=%s", host, user, dbName, password, dbPort)
+
+	// opening connection to database
+	db, err = gorm.Open(dialect, dbURI)
+
+	if err != nil {
+		log.Fatal(err)
+	} else {
+		fmt.Println("Successfully connected to Database")
+	}
+
+	defer db.Close()
+
+	// make migration to database
+
+	db.AutoMigrate(&Contact{})
+
+	db.Create(contact)
+
+	// API routes
+
+	router := mux.NewRouter()
+
+	router.HandleFunc("/contacts", getContacts).Methods("GET")
+	router.HandleFunc("/contact/{id}", getContact).Methods("GET")
+	router.HandleFunc("/create/contact", createContact).Methods("POST")
+	router.HandleFunc("/delete/contact/{id}", deleteContact).Methods("DELETE")
+
+	log.Fatal(http.ListenAndServe(":8080", router))
+}
+
+func getContacts(w http.ResponseWriter, r *http.Request) {
+	w.Header().Set("Content-Type", "application/json")
+	var contacts []Contact
+	db.Find(&contacts)
+	json.NewEncoder(w).Encode(&contacts)
+}
+
+func getContact(w http.ResponseWriter, r *http.Request) {
+	w.Header().Set("Content-Type", "application/json")
+	params := mux.Vars(r)
+
+	var contact Contact
+
+	db.First(&contact, params["id"])
+
+	json.NewEncoder(w).Encode(contact)
+}
+
+func createContact(w http.ResponseWriter, r *http.Request) {
+	w.Header().Set("Content-Type", "application/json")
+	var contact Contact
+	json.NewDecoder(r.Body).Decode(&contact)
+
+	createdContact := db.Create(&contact)
+	err = createdContact.Error
+	if err != nil {
+		json.NewEncoder(w).Encode(err)
+	} else {
+		json.NewEncoder(w).Encode(&contact)
+	}
+}
+
+func deleteContact(w http.ResponseWriter, r *http.Request) {
+	w.Header().Set("Content-Type", "application/json")
+	params := mux.Vars(r)
+
+	var contact Contact
+
+	db.First(&contact, params["id"])
+	db.Delete(&contact)
+
+	json.NewEncoder(w).Encode(&contact)
 }
